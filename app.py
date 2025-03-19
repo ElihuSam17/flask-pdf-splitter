37eccfd84d1a2f43637399d3662460b93215df9c
ElihuSam17 <elihusam17@hotmail.com>
2025-03-19 15:37:34 +0000

    Preparando despliegue en Render

--- app.py
+++ app.py
@@ -51,5 +51,6 @@ def process_pdf():
     
     return send_file(output_zip_path, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
